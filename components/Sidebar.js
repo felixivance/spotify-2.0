cd168bddecfae895196a6ffa6595a99dda49014c
Felix <felixrunye@gmail.com>
2021-12-06 14:00:57 +0300

    side bar build

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,35 +4,42 @@ import { HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon }
 function Sidebar() {
     return (
         <div>
-            <div className="p-5 text-sm border-r border-gray-900 space-y-4">
-                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer text-gray-500">
+            <div className="p-5 text-sm border-r border-gray-900 space-y-4 text-gray-500">
+                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer">
                     <HomeIcon className="h-5" />
                     <p>Home</p>
                 </button>
-                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer text-gray-500">
+                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer">
                     <SearchIcon className="h-5" />
                     <p>Search</p>
                 </button>
-                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer text-gray-500">
+                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer">
                     <LibraryIcon className="h-5" />
                     <p>Your Library</p>
                 </button>
                 <hr className="border-t-[0,1px] border-gray-900"/>
-                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer text-gray-500">
+                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer">
                     <PlusCircleIcon className="h-5" />
                     <p>Create Playlist</p>
                 </button>
-                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer text-gray-500">
+                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer">
                     <HeartIcon className="h-5" />
                     <p>Liked Songs</p>
                 </button>
-                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer text-gray-500">
+                <button className="flex items-center space-x-2 hover:text-white hover:cursor-pointer">
                     <RssIcon className="h-5" />
                     <p>Your Episodes</p>
                 </button>
                 <hr className="border-t-[0,1px] border-gray-900"/>
 
                 {/* playlists */}
+               {
+                [ ...Array(10).keys() ].map((i, index)=>
+                    <p className="cursor-pointer hover:text-white" key={index}>
+                        Playlist name...
+                    </p>
+                )    
+               }
             </div>
         </div>
     )
