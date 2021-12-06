b44bac1f14f392c4bbeeaed62f9f70e8daf11b9b
Felix <felixrunye@gmail.com>
2021-12-06 19:17:24 +0300

    updated loadash to shuffle colors on page reload

--- components/Center.js
+++ components/Center.js
@@ -1,8 +1,22 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react";
+import { shuffle } from 'lodash';
 
 function Center() {
     const { data:session} = useSession();
+    const [color, setColor] = useState(null);
+
+    const colors = [
+        "from-indigo-500",
+        "from-blue-500", "from-green-500",
+        "from-red-500","from-yellow-500","from-pink-500","from-purple-500"
+    ]
+
+    useEffect(()=>{
+        setColor(shuffle(colors).pop())
+    },[])
+
     return (
         <div className="flex-grow ">
             <header className="absolute top-5 right-8">
@@ -15,7 +29,7 @@ function Center() {
                 </div>
             </header>
 
-            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8 w-full`}>
+            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8 w-full`}>
                 {/* <img src="" alt="" /> */}
                 <p>Hello</p>
             </section>
