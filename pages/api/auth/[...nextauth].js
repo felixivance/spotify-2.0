5ed0ac5a8e3228fa0860d51393a62c03092422cd
Felix <felixrunye@gmail.com>
2021-12-06 17:45:40 +0300

    updated next auth authorization

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+import { LOGIN_URL } from "../../../lib/spotify"
 
 export default NextAuth({
   // Configure one or more authentication providers
