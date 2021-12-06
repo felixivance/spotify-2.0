d323a178de4c9aabb15ffca1a0ad83b626401d9f
Felix <felixrunye@gmail.com>
2021-12-06 18:39:27 +0300

    addition of middleware in next js -> realy cool

---
+++ pages/_middleware.js
@@ -0,0 +1,23 @@
+import  { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export async function middeware(req){
+    //token exists if user is loggedin
+    const token = await getToken({ req, secret:process.env.JWT_SECRET});
+    const { pathname } = req.nextUrl;
+
+    //allow the request if the following is true
+    //1. if its a request for next auth session and provider fetching
+    //2 if token exists
+
+    if(pathname.includes('/api/auth') || token){
+        return NextResponse.next();
+    }
+
+    //redirect them to login if they dont have a token and are requesting a protected route
+
+    if(!token && pathname !== '/login'){
+        return NextResponse.redirect('/login');
+    }
+
+}
