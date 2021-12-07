ffadd8e075a2bdd68391edcd34b60176fae542e6
Felix <felixrunye@gmail.com>
2021-12-07 16:49:40 +0300

    fetching playlist image

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -4,3 +4,8 @@ export const playlistIdState = atom({
     key:"playlistIdState",
     default:"0i60DjXj6Rj43Nv56QX9AG"
 });
+
+export const playlistState = atom({
+    key:"playlistState",
+    default:null
+})
--- components/Center.js
+++ components/Center.js
@@ -2,13 +2,16 @@ import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
-import {  useRecoilValue } from "recoil";
-import { playlistIdState } from "../atoms/playlistAtom";
+import {  useRecoilState, useRecoilValue } from "recoil";
+import { playlistIdState,playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
 
 function Center() {
   const { data: session } = useSession();
   const [color, setColor] = useState(null);
-  const playlistsId = useRecoilValue(playlistIdState);
+  const playlistId = useRecoilValue(playlistIdState);
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
+  const spotifyApi = useSpotify();
 
   const colors = [
     "from-indigo-500",
@@ -20,9 +23,16 @@ function Center() {
     "from-purple-500",
   ];
 
+
   useEffect(() => {
     setColor(shuffle(colors).pop());
-  }, [playlistsId]);
+  }, [playlistId]);
+
+  useEffect(()=>{
+    spotifyApi.getPlaylist(playlistId).then((data)=>{
+        setPlaylist(data.body)
+    }).catch((error)=> console.log("something went wrong getting the playlist ", error))  
+  },[spotifyApi, playlistId])
 
   return (
     <div className="flex-grow ">
@@ -41,7 +51,7 @@ function Center() {
       <section
         className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8 w-full`}
       >
-        {/* <img src="" alt="" /> */}
+        <img src={playlist?.images?.[0]?.url} alt="" className="h-44 w-44 shadow-2xl rounded-md" />
         <p>Hello</p>
       </section>
     </div>
