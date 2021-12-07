366d63b9393b29c13964757ff7df051d2e85c718
Felix <felixrunye@gmail.com>
2021-12-07 22:41:15 +0300

    added serverside props on home page to load user

--- components/Center.js
+++ components/Center.js
@@ -40,7 +40,7 @@ function Center() {
   return (
     <div className="flex-grow ">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-clack space-x-3 opacity-90 hover:opacity-80 rounded-full p-1 pr-2 bg-black">
+        <div className="flex items-center bg-clack space-x-3 opacity-90 hover:opacity-80 rounded-full p-1 pr-2 bg-gray-700">
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
+import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Center from "../components/Center";
 import Sidebar from "../components/Sidebar";
 
-export default function Home() {
+export default function Home({session}) {
   return (
     <div className="bg-black h-screen overflow-hidden">
       <Head>
-        <title>Spotify 2.0 | Felix Clone</title>
+        <title>Spotify 2.0 | Welcome</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -19,4 +20,15 @@ export default function Home() {
       {/* div for player */}
     </div>
   );
+}
+
+export async function getServerSideProps(context){
+
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
