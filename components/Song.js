0b142229a7cfbb2931c46720d80068a1122e8fa4
Felix <felixrunye@gmail.com>
2021-12-07 23:05:46 +0300

    updated playlist listing

--- components/Song.js
+++ components/Song.js
@@ -4,18 +4,18 @@ function Song({order, track}) {
     const spotifyApi = useSpotify();
 
     return (
-        <div className="grid grid-cols-2 ">
+        <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
             <div className="flex items-center space-x-4">
                 <p>{order +1 }</p>
                 <img className="h-10 w-10 rounded-md" src={track.track.album.images[0].url} alt="" />
                 <div>
-                    <p>{track.track.name}</p>
+                    <p className="w-36 lg:w-64 truncate text-white">{track.track.name}</p>
                     <p>{track.track.artists[0].name}</p>
                 </div>
             </div>
             <div className="flex items-center justify-between ml-auto md:ml-0">
-                <p className="hidden md:inline">{track.track.album.name}</p>
-                <p>{millistToMinutesAndSeconds(track.track.duration_ms)}</p>
+                <p className="hidden md:inline w-40">{track.track.album.name}</p>
+                <p className="">{millistToMinutesAndSeconds(track.track.duration_ms)}</p>
             </div>
         </div>
     )
