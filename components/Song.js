6d0d4b6e585f4426875909277db513096a8fffcf
Felix <felixrunye@gmail.com>
2021-12-07 23:30:20 +0300

    updated playing to spotify devices

--- components/Song.js
+++ components/Song.js
@@ -1,10 +1,25 @@
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify"
 
 function Song({order, track}) {
     const spotifyApi = useSpotify();
+    const [ currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+
+    const [ isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+    const playSong = () =>{
+        setCurrentTrackId(track.track.id);
+        setIsPlaying(true);
+
+        spotifyApi.play({
+            uris: [track.track.uri]
+        })
+    }
 
     return (
-        <div className="grid grid-cols-2 text-gray-500 py-3 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
+        <div className="grid grid-cols-2 text-gray-500 py-3 px-5 hover:bg-gray-900 rounded-lg cursor-pointer"
+        onClick={()=>playSong()}>
             <div className="flex items-center space-x-4">
                 <p>{order +1 }</p>
                 <img className="h-10 w-10 rounded-md" src={track.track.album.images[0].url} alt="" />
