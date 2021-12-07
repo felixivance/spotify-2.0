fd53ea6260e53317f30dd0f6f8f4343dc9ffeb7f
Felix <felixrunye@gmail.com>
2021-12-07 22:34:38 +0300

    updated header icon

--- components/Center.js
+++ components/Center.js
@@ -40,13 +40,13 @@ function Center() {
   return (
     <div className="flex-grow ">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-clack space-x-3 opacity-90 hover:opacity-80 rounded-full p-1 pr-2 bg-gray-300">
+        <div className="flex items-center bg-clack space-x-3 opacity-90 hover:opacity-80 rounded-full p-1 pr-2 bg-black">
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
             alt=""
           />
-          <h2>{session?.user.name}</h2>
+          <h2 className="text-white">{session?.user.name}</h2>
           <ChevronDownIcon className="h-5 w-5" />
         </div>
       </header>
