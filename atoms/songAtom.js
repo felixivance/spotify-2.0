c3ef07ae5e6f3c3c09fb86798e55e7886730b435
Felix <felixrunye@gmail.com>
2021-12-07 23:19:20 +0300

    song atom state creation

---
+++ atoms/songAtom.js
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+export const currentTrackIdState = atom({
+    key:"currentTrackIdState", //unique id with respect to other atoms,
+    default: null,
+
+})
+
+export const isPlayingState = atom({
+    key: 'isPlayingState',
+    default:false
+})
