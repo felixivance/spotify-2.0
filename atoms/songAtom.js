097195b390313fb24102385cd16bce66f563895f
Felix <felixrunye@gmail.com>
2021-12-08 17:03:43 +0300

    song atom page redesign

--- atoms/songAtom.js
+++ atoms/songAtom.js
@@ -1,12 +1,11 @@
 import { atom } from "recoil";
 
 export const currentTrackIdState = atom({
-    key:"currentTrackIdState", //unique id with respect to other atoms,
-    default: null,
-
-})
+  key: "currentTrackIdState", //unique id with respect to other atoms,
+  default: null,
+});
 
 export const isPlayingState = atom({
-    key: 'isPlayingState',
-    default:false
-})
+  key: "isPlayingState",
+  default: false,
+});
