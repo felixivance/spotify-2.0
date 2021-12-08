2bdb019083544aaad5a71a99f398ec02be7b7254
Felix <felixrunye@gmail.com>
2021-12-08 17:02:34 +0300

    middleware update formatting

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,23 +1,22 @@
-import  { getToken } from 'next-auth/jwt';
-import { NextResponse } from 'next/server';
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
 
-export async function middleware(req){
-    //token exists if user is loggedin
-    const token = await getToken({ req, secret:process.env.JWT_SECRET});
-    const { pathname } = req.nextUrl;
+export async function middleware(req) {
+  //token exists if user is loggedin
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  const { pathname } = req.nextUrl;
 
-    //allow the request if the following is true
-    //1. if its a request for next auth session and provider fetching
-    //2 if token exists
+  //allow the request if the following is true
+  //1. if its a request for next auth session and provider fetching
+  //2 if token exists
 
-    if(pathname.includes('/api/auth') || token){
-        return NextResponse.next();
-    }
+  if (pathname.includes("/api/auth") || token) {
+    return NextResponse.next();
+  }
 
-    //redirect them to login if they dont have a token and are requesting a protected route
+  //redirect them to login if they dont have a token and are requesting a protected route
 
-    if(!token && pathname !== '/login'){
-        return NextResponse.redirect('/login');
-    }
-
+  if (!token && pathname !== "/login") {
+    return NextResponse.redirect("/login");
+  }
 }
