5596816affba17e98b1fe63d8dd9131500ad682f
Felix <felixrunye@gmail.com>
2021-12-08 17:04:49 +0300

    updated center component design

--- components/Center.js
+++ components/Center.js
@@ -2,8 +2,8 @@ import { ChevronDownIcon } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
-import {  useRecoilState, useRecoilValue } from "recoil";
-import { playlistIdState,playlistState } from "../atoms/playlistAtom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
 import useSpotify from "../hooks/useSpotify";
 import Songs from "./Songs";
 
@@ -24,24 +24,30 @@ function Center() {
     "from-purple-500",
   ];
 
-
   useEffect(() => {
     setColor(shuffle(colors).pop());
   }, [playlistId]);
 
-  useEffect(()=>{
-    spotifyApi.getPlaylist(playlistId).then((data)=>{
-        setPlaylist(data.body)
-    }).catch((error)=> console.log("something went wrong getting the playlist ", error))  
-  },[spotifyApi, playlistId])
+  useEffect(() => {
+    spotifyApi
+      .getPlaylist(playlistId)
+      .then((data) => {
+        setPlaylist(data.body);
+      })
+      .catch((error) =>
+        console.log("something went wrong getting the playlist ", error)
+      );
+  }, [spotifyApi, playlistId]);
 
   console.log(playlist);
 
   return (
     <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-clack space-x-3 opacity-90 hover:opacity-80 rounded-full p-1 pr-2 bg-gray-700 cursor-pointer"
-         onClick={() => signOut()}>
+        <div
+          className="flex items-center bg-clack space-x-3 opacity-90 hover:opacity-80 rounded-full p-1 pr-2 bg-gray-700 cursor-pointer"
+          onClick={() => signOut()}
+        >
           <img
             className="rounded-full w-10 h-10"
             src={session?.user.image}
@@ -55,14 +61,20 @@ function Center() {
       <section
         className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8 w-full`}
       >
-        <img src={playlist?.images?.[0]?.url} alt="" className="h-44 w-44 shadow-2xl rounded-md" />
+        <img
+          src={playlist?.images?.[0]?.url}
+          alt=""
+          className="h-44 w-44 shadow-2xl rounded-md"
+        />
         <div className="flex flex-col">
-            <p className="tracking-wider">PLAYLIST</p>
-            <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
+          <p className="tracking-wider">PLAYLIST</p>
+          <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">
+            {playlist?.name}
+          </h1>
         </div>
       </section>
       <div>
-          <Songs />
+        <Songs />
       </div>
     </div>
   );
