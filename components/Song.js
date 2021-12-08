c1f04f464bede0f21a18840f883b7c41eb4969d2
Felix <felixrunye@gmail.com>
2021-12-08 13:47:19 +0300

    updated songs list

--- components/Song.js
+++ components/Song.js
@@ -20,7 +20,7 @@ function Song({order, track}) {
     return (
         <div className="grid grid-cols-2 text-gray-500 py-3 px-5 hover:bg-gray-900 rounded-lg cursor-pointer"
         onClick={()=>playSong()}>
-            <div className="flex items-center space-x-4">
+            <div className="flex items-center space-x-4 ">
                 <p>{order +1 }</p>
                 <img className="h-10 w-10 rounded-md" src={track.track.album.images[0].url} alt="" />
                 <div>
