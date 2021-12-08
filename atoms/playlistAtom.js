a26b2265334530add3bcf2f256ce2307f0db6566
Felix <felixrunye@gmail.com>
2021-12-08 17:03:30 +0300

    atom page redesign

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -1,11 +1,11 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
 
 export const playlistIdState = atom({
-    key:"playlistIdState",
-    default:"0i60DjXj6Rj43Nv56QX9AG"
+  key: "playlistIdState",
+  default: "0i60DjXj6Rj43Nv56QX9AG",
 });
 
 export const playlistState = atom({
-    key:"playlistState",
-    default:null
-})
+  key: "playlistState",
+  default: null,
+});
