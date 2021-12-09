0af25349afd397fe2c590a6a7436703efac194fb
Felix <felixrunye@gmail.com>
2021-12-09 20:32:50 +0300

    use song info update

--- components/Player.js
+++ components/Player.js
@@ -2,6 +2,7 @@ import { useSession } from "next-auth/react";
 import { useRecoilState } from "recoil";
 import useSpotify from "../hooks/useSpotify";
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
+import { useState } from "react";
 
 function Player() {
 
@@ -9,7 +10,7 @@ function Player() {
     const { data: session, status } = useSession();
     const [ currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState)
     const [ isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
-
+    const [ volume, setVolume] = useState(50)
     return (
         <div className="">
             {/* left */}
---
+++ hooks/useSongInfo.js
@@ -0,0 +1,6 @@
+
+function useSongInfo(){
+
+}
+
+export default useSongInfo
