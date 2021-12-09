84c5ae28da7e748362ec26834022d6f69f3742d4
Felix <felixrunye@gmail.com>
2021-12-09 20:30:10 +0300

    player update

--- components/Player.js
+++ components/Player.js
@@ -1,8 +1,14 @@
+import { useSession } from "next-auth/react";
+import { useRecoilState } from "recoil";
 import useSpotify from "../hooks/useSpotify";
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 
 function Player() {
 
     const spotifyApi = useSpotify();
+    const { data: session, status } = useSession();
+    const [ currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState)
+    const [ isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
 
     return (
         <div className="">
