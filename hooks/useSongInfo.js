e5009b21c8d3a1402c038e188e60601aa503a079
Felix <felixrunye@gmail.com>
2021-12-09 20:36:06 +0300

    use song info hook creation

--- hooks/useSongInfo.js
+++ hooks/useSongInfo.js
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState } from "../atoms/songAtom";
+import useSpotify from "./useSpotify"
 
 function useSongInfo(){
+    const spotifyApi = useSpotify();
+    const [ currentTrackId, setCurrentTrackId ] = useRecoilState(currentTrackIdState)
+    const [ songInfo, setSongInfo] = useState(null);
 
+
+    useEffect(()=>{
+        const fetchSongInfo = async ()=>{
+            
+        }
+    },[currentTrackId, spotifyApi])
+
+    return songInfo;
 }
 
 export default useSongInfo
