7661d55eceebf8a3e15cba4ad9d05d41711383f9
Felix <felixrunye@gmail.com>
2021-12-09 21:23:37 +0300

    use song info

--- hooks/useSongInfo.js
+++ hooks/useSongInfo.js
@@ -1,31 +1,32 @@
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { currentTrackIdState } from "../atoms/songAtom";
-import useSpotify from "./useSpotify"
+import useSpotify from "./useSpotify";
 
-function useSongInfo(){
-    const spotifyApi = useSpotify();
-    const [ currentTrackId, setCurrentTrackId ] = useRecoilState(currentTrackIdState)
-    const [ songInfo, setSongInfo] = useState(null);
+function useSongInfo() {
+  const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [songInfo, setSongInfo] = useState(null);
 
+  useEffect(() => {
+    const fetchSongInfo = async () => {
+      if (currentTrackId) {
+        const trackInfo = await fetch(
+          `https://api.spotify.com/v1/tracks/${currentTrackId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${spotifyApi.getAccessToken()}`,
+            },
+          }
+        ).then((res) => res.json());
 
-    useEffect(()=>{
-        const fetchSongInfo = async ()=>{
-            if(currentTrackId){
-                const trackInfo = await fetch(
-                    `https://api.spotify.com/v1/tracks/${currentTrackId}`,{
-                        headers:{
-                            Authorization: `Bearer ${spotifyApi.getAccessToken()}`
-                        }
-                    }
-                ).then((res)=>res.json())
+        setSongInfo(trackInfo);
+      }
+    };
+  }, [currentTrackId, spotifyApi]);
 
-                setSongInfo(trackInfo);
-            }
-        }
-    },[currentTrackId, spotifyApi])
-
-    return songInfo;
+  return songInfo;
 }
 
-export default useSongInfo
+export default useSongInfo;
