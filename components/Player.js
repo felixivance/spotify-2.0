c087aad79a9a3c84638e3d3430296ddc53f764e1
Felix <felixrunye@gmail.com>
2021-12-10 20:43:18 +0300

    player update

--- components/Player.js
+++ components/Player.js
@@ -4,6 +4,8 @@ import useSpotify from "../hooks/useSpotify";
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
 import { useEffect, useState } from "react";
 import useSongInfo from "../hooks/useSongInfo";
+import {  SwitchHorizontalIcon, HeartIcon, VolumeUpIcon as VolumeDownIcon, PlayIcon } from "@heroicons/react/outline";
+import { RewindIcon, FastForwardIcon, PauseIcon, ReplyIcon, VolumeUpIcon } from "@heroicons/react/solid";
 
 function Player() {
 
@@ -36,6 +38,18 @@ function Player() {
         }
     },[currentTrackIdState, spotifyApi, session])
 
+    const handlePlayPause=()=>{
+        spotifyApi.getMyCurrentPlaybackState().then((data)=>{
+            if(data.body.is_playing){
+                spotifyApi.pause();
+                setIsPlaying(false);
+            }else{
+                spotifyApi.play();
+                setIsPlaying(true);
+            }
+        })
+    }
+
     return (
         <div className="text-white bg-gradient-to-b from-black to-gray-900 h-24 grid grid-cols-3 text-xs md:text-base px-2 md:px-8 ">
             {/* left */}
@@ -45,6 +59,27 @@ function Player() {
                     <h3 className=""> {songInfo?.name} </h3>
                     <p>{songInfo?.artists?.[0]?.name}</p>
                 </div>
+            </div>
+
+            {/* center */}
+            <div className="flex items-center justify-evenly">
+                <SwitchHorizontalIcon className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
+                <RewindIcon 
+                // onClick={()=> spotifyApi.skipToPrevious()}  ->api not working
+                className=" h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"/>
+                {
+                    isPlaying ? (
+                        <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+                    ) : (
+                        <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+                    )
+                }
+
+                <FastForwardIcon 
+                // onClick={()=>spotifyApi.skipToNext()} api not working
+                className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
+
+                <ReplyIcon className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
             </div>
             
             
